bb6fc401f33fd70e9f9936e360afeabad7fbc225
Axel <axel.lindroos1@gmail.com>
2024-10-30 11:48:37 +0200

    tests

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -10,6 +10,16 @@ class TestVarasto(unittest.TestCase):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
+    def test_neg_konstruktori(self):
+        self.virheellinen = Varasto(-1, -1)
+        self.assertAlmostEqual(self.virheellinen.tilavuus, 0)
+        self.assertAlmostEqual(self.virheellinen.saldo,0)
+
+    def test_liika_saldo(self):
+        self.virhe = Varasto(5, 8)
+        self.assertAlmostEqual(self.virhe.saldo, self.virhe.tilavuus)
+    
+
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
 
@@ -17,6 +27,16 @@ class TestVarasto(unittest.TestCase):
         self.varasto.lisaa_varastoon(8)
 
         self.assertAlmostEqual(self.varasto.saldo, 8)
+    
+    def test_liika_lisays(self):
+        self.varasto.lisaa_varastoon(20)
+        
+        self.assertAlmostEqual(self.varasto.saldo, self.varasto.tilavuus)
+
+    def test_neg_lisays(self):
+        holder = self.varasto.saldo
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, holder)
 
     def test_lisays_lisaa_pienentaa_vapaata_tilaa(self):
         self.varasto.lisaa_varastoon(8)
@@ -38,3 +58,15 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+    
+    def test_ota_neg(self):
+        self.assertAlmostEqual(self.varasto.ota_varastosta(-1), 0.0)
+
+    def test_liikaa(self):
+        s= self.varasto.saldo
+        x = self.varasto.ota_varastosta(20)
+        self.assertAlmostEqual(s, x)
+
+    def test_print(self):
+        s = self.varasto.__str__()
+        self.assertAlmostEqual(f"saldo = {self.varasto.saldo}, vielä tilaa {self.varasto.paljonko_mahtuu()}", s )
--- src/varasto.py
+++ src/varasto.py
@@ -4,17 +4,17 @@ class Varasto:
             self.tilavuus = tilavuus
         else:
             # virheellinen, nollataan
-            self.tilavuus = 0.0
+            self.tilavuus = 0.0  #
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
-            self.saldo = 0.0
+            self.saldo = 0.0 #
         elif alku_saldo <= tilavuus:
             # mahtuu
             self.saldo = alku_saldo
         else:
             # täyteen ja ylimäärä hukkaan!
-            self.saldo = tilavuus
+            self.saldo = tilavuus #
 
     # huom: ominaisuus voidaan myös laskea. Ei tarvita erillistä kenttää viela_tilaa tms.
     def paljonko_mahtuu(self):
@@ -30,16 +30,16 @@ class Varasto:
 
     def ota_varastosta(self, maara):
         if maara < 0:
-            return 0.0
+            return 0.0 #
         if maara > self.saldo:
-            kaikki_mita_voidaan = self.saldo
+            kaikki_mita_voidaan = self.saldo #
             self.saldo = 0.0
 
-            return kaikki_mita_voidaan
+            return kaikki_mita_voidaan #
 
         self.saldo = self.saldo - maara
 
         return maara
 
     def __str__(self):
-        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}"
+        return f"saldo = {self.saldo}, vielä tilaa {self.paljonko_mahtuu()}" #
